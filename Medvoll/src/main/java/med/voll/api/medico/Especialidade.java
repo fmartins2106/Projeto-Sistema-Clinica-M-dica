5b323bf4c3f12bc100c8c079bc5d5cf772037193
Fernando Martins <fernandom.adm@gmail.com>
2025-08-05 12:45:06 -0300

    RequestMapping

---
+++ Medvoll/src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,15 @@
+package med.voll.api.medico;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA;
+
+    @JsonCreator
+    public static Especialidade fromString(String value){
+        return Especialidade.valueOf(value.toUpperCase());
+    }
+}
