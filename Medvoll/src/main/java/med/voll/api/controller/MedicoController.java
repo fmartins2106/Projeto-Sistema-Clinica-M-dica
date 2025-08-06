260bf659361d6d5d4282b96b13c98198313bf41f
Fernando Martins <fernandom.adm@gmail.com>
2025-08-06 17:11:38 -0300

    Adicionando funcionalidades de listar, atualizar e excluir para medicos

--- Medvoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ Medvoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -18,8 +21,38 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
-
         repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public Page<DadosListagemMedicos> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarDadosMedico(@RequestBody @Valid DadosAtualizacaoMedicos dadosAtualizacaoMedicos){
+        var medico = repository.getReferenceById(dadosAtualizacaoMedicos.id());
+        medico.atualizarInformacoes(dadosAtualizacaoMedicos);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
+
+
+
+
+
+    // Metodo abaixo serve para poder excluir de forma total do banco de dados e do sistema.
+//    @DeleteMapping("{id}")
+//    @Transactional
+//    public void excluirDadosMedico(@PathVariable Long id){
+//        repository.deleteById(id);
+//    }
 
 }
--- Medvoll/src/main/java/med/voll/api/medico/Medico.java
+++ Medvoll/src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -28,12 +29,32 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedicos dadosAtualizacaoMedicos) {
+        if (dadosAtualizacaoMedicos.nome() != null){
+            this.nome = dadosAtualizacaoMedicos.nome();
+        }
+        if (dadosAtualizacaoMedicos.telefone() != null){
+            this.telefone = dadosAtualizacaoMedicos.telefone();
+        }
+        if (dadosAtualizacaoMedicos.endereco() != null){
+            this.endereco.atualizarEndereco(dadosAtualizacaoMedicos.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
