c3d6e8bf4aad1d4db7340cb053c828b8603b1708
Fernando Martins <fernandom.adm@gmail.com>
2025-08-09 19:43:31 -0300

    Finalizando implementação de autenticação

--- Medvoll/src/main/java/med/voll/api/infra/security/TokenService.java
+++ Medvoll/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,7 +3,9 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.*;
@@ -11,10 +13,13 @@ import java.util.Date;
 
 @Service
 public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String gerarToken(Usuario usuario){
+        System.out.println(secret);
         try {
-            var algoritimo = Algorithm.HMAC256("12345678");
+            var algoritimo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
@@ -25,9 +30,24 @@ public class TokenService {
         }
     }
 
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    // specify any specific claim validations
+                    .withIssuer("API Voll.med")
+                    // reusable verifier instance
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado.");
+        }
+    }
+
     private Instant dataExpiracao() {
         return ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
-                .plusHours(2)
+                .plusHours(6)
                 .toInstant();
     }
 
